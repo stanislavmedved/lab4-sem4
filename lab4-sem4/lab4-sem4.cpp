aa655c85375d6653fe8b751cb9263a394a1232e3
stanislavmedved <145404670+stanislavmedved@users.noreply.github.com>
2024-05-21 01:40:24 +0300

    Initial commit

---
+++ lab4-sem4/lab4-sem4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <concepts>
+#include <type_traits>
+
+template<typename T>
+concept valid_numeric = std::is_integral<T>::value || std::is_floating_point<T>::value;
+
+template<valid_numeric T>
+int findSecondMaxIndex(T *arr, unsigned int size) {
+    T maxElement = arr[0];
+    //find max index
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+    T secondMax = arr[0];
+    int resultIndex = 0;
+    // find second max
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > maxElement) {
+            resultIndex = i;
+            secondMax = arr[i];
+        }
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
